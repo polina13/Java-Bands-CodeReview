def08eadaa70aa4277341c9ea6652739b05c91ed
Polina Nenchev <polina.mihova@yahoo.com>
2016-03-04 08:01:42 -0800

    Fixing routing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -33,13 +33,62 @@ public class App {
     }, new VelocityTemplateEngine());
 
     get("/venues/:id", (request,response) -> {
-    HashMap<String, Object> model = new HashMap<String, Object>();
-    int id = Integer.parseInt(request.params("id"));
-    Venue venue = Venue.find(id);
-    model.put("venue", venue);
-    model.put("allBands", Band.all());
-    model.put("template", "templates/venue.vtl");
-    return new ModelAndView(model, layout);
-  }, new VelocityTemplateEngine());
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      int id = Integer.parseInt(request.params("id"));
+      Venue venue = Venue.find(id);
+      model.put("venue", venue);
+      model.put("allBands", Band.all());
+      model.put("template", "templates/venue.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/bands/:id", (request,response) ->{
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      int id = Integer.parseInt(request.params("id"));
+      Band band = Band.find(id);
+      model.put("band", band);
+      model.put("allVenues", Venue.all());
+      model.put("template", "templates/band.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/venues", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      Venue newVenue = new Venue(name);
+      newVenue.save();
+      response.redirect("/venues");
+      return null;
+    });
+
+
+    post("/bands", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      Band newBand = new Band(name);
+      newBand.save();
+      response.redirect("/bands");
+      return null;
+    });
+
+    post("/add_venues", (request, response) -> {
+      int venueId = Integer.parseInt(request.queryParams("venue_id"));
+      int bandId = Integer.parseInt(request.queryParams("band_id"));
+      Band band = Band.find(bandId);
+      Venue venue = Venue.find(venueId);
+      band.addVenue(venue);
+      response.redirect("/bands/" + bandId);
+      return null;
+    });
+
+    post("/add_bands", (request, response) -> {
+      int venueId = Integer.parseInt(request.queryParams("venue_id"));
+      int bandId = Integer.parseInt(request.queryParams("band_id"));
+      Band band = Band.find(bandId);
+      Venue venue = Venue.find(venueId);
+      venue.addBand(band);
+      response.redirect("/venues/" + venueId);
+      return null;
+    });
   }
 }
