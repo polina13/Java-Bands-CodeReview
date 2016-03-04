cf48193ff39a13ff8b9838c86da9e57d34bbc738
Polina Nenchev <polina.mihova@yahoo.com>
2016-03-04 07:43:40 -0800

    Pass integration test for creating band object

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,5 +15,21 @@ public class App {
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/bands", (request, response) -> {
+      HashMap<String, Object>model = new HashMap<String, Object>();
+      List<Band> bands = Band.all();
+      model.put("bands", bands);
+      model.put("template", "templates/bands.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/venues", (request, response) -> {
+      HashMap<String, Object>model = new HashMap<String, Object>();
+      List<Venue> venues = Venue.all();
+      model.put("venues", venues);
+      model.put("template", "templates/venues.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
-}  
+}
